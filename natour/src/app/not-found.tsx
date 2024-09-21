04a1b482a24a32f48d3b83f442fd3e5e1e677b4b
FlamingoLindo <101421364+FlamingoLindo@users.noreply.github.com>
2024-09-21 12:38:00 -0300

    teste

--- natour/src/app/not-found.tsx
+++ natour/src/app/not-found.tsx
@@ -2,7 +2,7 @@ export default function NotFound() {
     return(
         <div>
             <h2>Página não encontrada</h2>
-            <p>Não voi possivel encontrar essa página</p>
+            <p>Não foi possivel encontrar essa página</p>
         </div>
     )
 }
